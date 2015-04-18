096ce1f107231c915317720c3c284c00378a98fb
Mike Gilbert <mkg52@dantooine.(none)>
2015-04-18 13:25:47 -0700

    Created procedure to add a new request. Changed request dispatcher to varchar so I could populate the request with the current logged in user as the dispatcher. The request uses sysdate as create_time. Also added an item_price column to menu_item table so we can have a total price on the request.

--- seed/menu_item.sql
+++ seed/menu_item.sql
@@ -2,6 +2,8 @@ CREATE TABLE menu_item(
     item_id INT CONSTRAINT menu_item_pk PRIMARY KEY DEFERRABLE INITIALLY IMMEDIATE,
     res_id INT CONSTRAINT menu_item_res_id_nn_cnst NOT NULL,
     item_name VARCHAR(255) CONSTRAINT menu_item_item_name_nn_cnst NOT NULL,
+    --added this line because we need prices to make sales right?
+    item_price NUMBER(9,2),
     item_description VARCHAR(255),
     CONSTRAINT menu_item_res_id_fk FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
 );
@@ -19,30 +21,30 @@ BEGIN
 END;
 /
 
-INSERT INTO menu_item (res_id, item_name, item_description)
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
 VALUES (1, 'Hunka Hunka Burnin'' Love', 
-        'Sliced Rocky Mountain Oyster glazed with a tangy Habanero glaze');
+        'Sliced Rocky Mountain Oyster glazed with a tangy Habanero glaze', 12.99);
 		
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (1, 'Love Me Tender', 'Seared beef tenderloin with sauteed mushrooms');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (1, 'Love Me Tender', 'Seared beef tenderloin with sauteed mushrooms', 17.99);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (1, 'Hound Dog', 'Dachshund sausage');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (1, 'Hound Dog', 'Dachshund sausage', 5.50);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (2, 'All Shook Up', '180 ounces of milk-shake, all flavors');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (2, 'All Shook Up', '180 ounces of milk-shake, all flavors', 3.99);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (2, 'Alright, Okay, You Win', '64 slices of bacon wrapped in... bacon');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (2, 'Alright, Okay, You Win', '64 slices of bacon wrapped in...bacon', 29.99);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (2, 'I''ll Never Know', 'Mystery meat-loaf');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (2, 'I''ll Never Know', 'Mystery meat-loaf', 11.95);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (3, 'Slice of Heaven', 'Pecan Pumpkin Pie');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (3, 'Slice of Heaven', 'Pecan Pumpkin Pie', 5.99);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (3, 'Turducken Slammer', 'Perfect storm of the top 3 edible birds');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (3, 'Turducken Slammer', 'Perfect storm of the top 3 edible birds', 19.99);
 
-INSERT INTO menu_item (res_id, item_name, item_description)
-VALUES (3, 'Cannibal''s Clown Burger', 'Does this taste funny to you');
+INSERT INTO menu_item (res_id, item_name, item_description, item_price)
+VALUES (3, 'Cannibal''s Clown Burger', 'Does this taste funny to you', 4.99);
--- seed/request.sql
+++ seed/request.sql
@@ -1,7 +1,8 @@
 CREATE TABLE request(
     request_id INT CONSTRAINT request_pk PRIMARY KEY,
     cus_loc_id INT CONSTRAINT request_cus_loc_id_nn_cnst NOT NULL,
-    dispatcher_id INT,
+    --prc_request_add procedure adds the current logged in user to this column
+    dispatcher VARCHAR2(20),
     driver_id INT,
     create_time DATE CONSTRAINT request_create_time_nn_cnst NOT NULL,
     dispatch_time DATE,
@@ -9,7 +10,6 @@ CREATE TABLE request(
     delivery_pickup DATE,
     delivery_complete DATE,
     CONSTRAINT cus_loc_id_fk FOREIGN KEY (cus_loc_id) REFERENCES customer_location(cus_loc_id),
-    CONSTRAINT request_dispatcher_fk FOREIGN KEY (dispatcher_id) REFERENCES employee(emp_id),
     CONSTRAINT request_driver_fk FOREIGN KEY (driver_id) REFERENCES employee(emp_id)
 );
 
