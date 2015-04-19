fa86371306e938daced8414ad917dc6eede01fd4
Jason Travis <infomaniac7@gmail.com>
2015-04-19 15:43:46 -0700

    Add employee schedule_view.sql

---
+++ views/schedule_view.sql
@@ -0,0 +1,16 @@
+-- This view will display the employee names next to their schedule start/end times displayed in international date format
+-- yyyy-mm-dd HH:MM
+-- EMP_ID EMP_FNAME EMP_LNAME SCHE_STAR SCHE_END
+
+-- Format columns to avoid wrapping.
+SET WRAP OFF
+SET LINESIZE 3000
+COLUMN emp_fname FORMAT A15
+COLUMN emp_lname FORMAT A15
+COLUMN emp_phone FORMAT A15
+COLUMN emp_email FORMAT A15
+
+CREATE OR REPLACE VIEW schedule_view AS
+    SELECT emp_id, emp_fname, emp_lname, (TO_CHAR(sche_start, 'yyyy-mm-dd hh24:mi')) "start", TO_CHAR(sche_end, 'yyyy-mm-dd hh24:mi') "end"
+    FROM employee
+    LEFT OUTER JOIN schedule USING (emp_id);
