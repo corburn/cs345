47941024724c2feaf8881e3ae067a8b2c211d0fe
Jason Travis <infomaniac7@gmail.com>
2015-04-19 15:25:13 -0700

    Fix dates of schedule seed

--- seed/schedule.sql
+++ seed/schedule.sql
@@ -20,7 +20,7 @@ END;
 /
 
 INSERT ALL
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (1, 1, '2015-03-29 08:00', '2015-03-29 12:00')
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (2, 1, '2015-03-29 13:00', '2015-03-29 17:00')
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (3, 1, '2015-03-29 08:00', '2015-03-29 12:00')
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (1, 1, TO_DATE('2015-03-29 08:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (2, 1, TO_DATE('2015-03-29 13:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (3, 1, TO_DATE('2015-03-29 08:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'))
 SELECT 1 FROM dual;
