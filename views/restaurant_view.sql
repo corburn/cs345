01fa8c57d7f5f187828d24d053789d204fad7125
Jackson Klinefelter <jak287@nau.edu>
2015-04-20 16:05:17 -0700

    Update restaurant_view.sql

--- views/restaurant_view.sql
+++ views/restaurant_view.sql
@@ -1,6 +1,11 @@
+--View for restaurants to see how frequently menu items have been ordered
+--Calling on the view as is will show all restaurants
+--To show a single restaurant then a WHERE statement is added when view is called
+--SELECT * FROM restaurant_view WHERE res_id = [restaurant ID];
+
 CREATE OR REPLACE VIEW restaurant_view AS
-	SELECT item_name, COUNT(request_id) AS Times_Ordered
+	SELECT item_name, res_id, COUNT(request_id) AS Times_Ordered
 	FROM Menu_Item A
 	FULL OUTER JOIN Request_Item B
 	ON A.item_id=B.item_id
-	GROUP BY item_name;
+	GROUP BY item_name, res_id;
