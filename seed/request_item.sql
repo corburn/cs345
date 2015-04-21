2a4595786ba7f4da8f8154f8532967c539940e10
Jason Travis <infomaniac7@gmail.com>
2015-04-20 19:16:29 -0700

    Add foreign key constraints to request_item

--- seed/request_item.sql
+++ seed/request_item.sql
@@ -2,5 +2,7 @@ CREATE TABLE request_item(
     item_id INT,
     request_id INT,
     quantity INT,
-    CONSTRAINT request_item_pk PRIMARY KEY (item_id, request_id)
+    CONSTRAINT request_item_pk PRIMARY KEY (item_id, request_id),
+    CONSTRAINT request_item_item_id_fk FOREIGN KEY (item_id) REFERENCES menu_item(item_id),
+    CONSTRAINT request_item_request_id_fk FOREIGN KEY (request_id) REFERENCES request(request_id)
 );
