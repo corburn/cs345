3057b2cacc0044fc36bcdba2f43dbd290a94fde2
Jackson Klinefelter <jak287@nau.edu>
2015-04-20 17:56:54 -0700

    Update schedule.sql
    Added more employee schedules that cover March 29 and 30.

--- seed/schedule.sql
+++ seed/schedule.sql
@@ -20,7 +20,18 @@ END;
 /
 
 INSERT ALL
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (1, 1, TO_DATE('2015-03-29 08:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'))
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (2, 1, TO_DATE('2015-03-29 13:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'))
-INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (3, 1, TO_DATE('2015-03-29 08:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (1, 1, TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 16:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (2, 1, TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 20:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (3, 2, TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (4, 2, TO_DATE('2015-03-29 18:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 22:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (5, 4, TO_DATE('2015-03-29 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (6, 4, TO_DATE('2015-03-29 17:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 22:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (7, 5, TO_DATE('2015-03-29 14:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-29 22:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (8, 1, TO_DATE('2015-03-30 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 16:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (9, 1, TO_DATE('2015-03-30 17:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 20:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (10, 3, TO_DATE('2015-03-30 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 17:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (11, 3, TO_DATE('2015-03-30 18:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 22:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (12, 4, TO_DATE('2015-03-30 12:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 17:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (13, 4, TO_DATE('2015-03-30 17:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 22:00', 'yyyy-mm-dd hh24:mi'))
+INTO schedule(sche_id, emp_id, sche_start, sche_end) VALUES (14, 6, TO_DATE('2015-03-30 14:00', 'yyyy-mm-dd hh24:mi'), TO_DATE('2015-03-30 22:00', 'yyyy-mm-dd hh24:mi'))
 SELECT 1 FROM dual;
