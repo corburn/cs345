954efffbc589403d51d639061fa3d841db2f5fb0
Jackson Klinefelter <jak287@nau.edu>
2015-04-20 19:16:25 -0700

    Update menu_item.sql

--- seed/menu_item.sql
+++ seed/menu_item.sql
@@ -1,10 +1,10 @@
 CREATE TABLE menu_item(
     item_id INT CONSTRAINT menu_item_pk PRIMARY KEY DEFERRABLE INITIALLY IMMEDIATE,
     res_id INT CONSTRAINT menu_item_res_id_nn_cnst NOT NULL,
-    item_name VARCHAR(255) CONSTRAINT menu_item_item_name_nn_cnst NOT NULL,
+    item_name VARCHAR(30) CONSTRAINT menu_item_item_name_nn_cnst NOT NULL,
     --added this line because we need prices to make sales right?
     item_price NUMBER(9,2),
-    item_description VARCHAR(255),
+    item_description VARCHAR(50),
     CONSTRAINT menu_item_res_id_fk FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
 );
 
