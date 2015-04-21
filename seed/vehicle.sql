f9a6d0076d8272f5e1488f8af493cec5e312deef
Jackson Klinefelter <jak287@nau.edu>
2015-04-20 17:02:54 -0700

    Update vehicle.sql
    Add more vehicles to allow for more diverse table calls.

--- seed/vehicle.sql
+++ seed/vehicle.sql
@@ -26,6 +26,8 @@ INSERT ALL
 INTO vehicle(veh_id, emp_id, veh_make, veh_model, veh_year, veh_color, veh_lic_plt) VALUES (1, 1, 'Stutz', 'Blackhawk', 1971, 'Black', 'HHBL9')
 INTO vehicle(veh_id, emp_id, veh_make, veh_model, veh_year, veh_color, veh_lic_plt) VALUES (2, 2, 'Cadillac', 'Fleetwood', 1955, 'Pink', 'LPN9')
 INTO vehicle(veh_id, emp_id, veh_make, veh_model, veh_year, veh_color, veh_lic_plt) VALUES (3, 2, 'Ford', 'Thunderbird', 1967, 'Red', 'YANBAHD9')
+INTO vehicle(veh_id, emp_id, veh_make, veh_model, veh_year, veh_color, veh_lic_plt) VALUES (4, 3, 'Isuzu', 'Rodeo', 1991, 'White', 'AAW392')
+INTO vehicle(veh_id, emp_id, veh_make, veh_model, veh_year, veh_color, veh_lic_plt) VALUES (5, 5, 'GMC', 'Sierra', 2006, 'Black', 'CLL445')
 SELECT 1 FROM dual;
 
 
